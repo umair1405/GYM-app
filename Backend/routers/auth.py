19669156259f402df61d11e54c98f331468b2c38
umair <umair1405@users.noreply.github.com>
2025-11-30 11:37:36 +0530

    your message

--- Backend/routers/auth.py
+++ Backend/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form
 from passlib.hash import bcrypt
 from sqlalchemy.orm import Session
 from database import SessionLocal
@@ -14,8 +14,14 @@ def get_db():
         db.close()
 
 @router.post("/login")
-def login(email: str, password: str, db: Session = Depends(get_db)):
+def login(
+    email: str = Form(...),
+    password: str = Form(...),
+    db: Session = Depends(get_db)
+):
     user = db.query(models.User).filter(models.User.email == email).first()
+
     if user and bcrypt.verify(password, user.password):
         return {"status": "success", "user_id": user.id, "name": user.name}
+
     return {"status": "error", "msg": "Invalid credentials"}
